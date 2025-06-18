b97ce9d40712e4be90efc89e406da833a32a86cd
Fabiha Hashmat <fhashmat@purdue.edu>
2025-06-18 14:26:59 -0400

    chore: lowercase normalization of resolved package names from Stage B

--- SecElf_StageC_PackageToCVE.py
+++ SecElf_StageC_PackageToCVE.py
@@ -12,23 +12,32 @@ import re               # For basic regex parsing
 #    - package_name
 #    - package_version
 #Accessing the resolved package column from the stage B
+#This code is also normalizing
 resolved_packages = [] # created an empty list to store all the resolved packages got from the stage B column resolved packages
 with open("library_packages.csv", "r") as f: # Open the Stage B CSV file 
     reader = csv.DictReader(f) #Use DictReader to read the CSV specific column
     for row in reader: # Loop through each row in the csv file
         resolved = row.get("ResolvedPackage", "").strip() # remove any leading or trailing spaces
         if resolved: #just storing the values of resolved packages if they are present
-            resolved_packages.append(resolved)
+            resolved_packages.append(resolved.lower())
 
-#
+
+# FOR NOW THIS PART COULD BE SKIPPED. LATER IF WE NEED TO NORMALIZE MORE WE CAN DEFINE A FUNCTION HERE.
 # 2. Normalize package names and versions if necessary:
 #    - Convert package names to lowercase
 #    - Optionally strip distribution-specific suffixes from version strings
+#if the package version = 1.1.1f-1ubuntu2.16" it will convert it to 1.1.1f"
+#if the package version is package_version = "1.2.8" then it will remain like 1.2.8"
+
+
+
 #
-# 3. For each (package_name, package_version) entry: 
+# 3. For each (resolved package) entry: 
 # In this we will be working on the NVD database only for now. 
 #    a. Query the NVD API using package name and version
 #       - Extract CVE ID, CVSS score, description, and reference URL (URL is not required for now)
+#
+
 
 
 #    b. Scrape the Debian Security Tracker for the source package
