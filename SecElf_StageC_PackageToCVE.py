b4f5cf008367c8f07eaf0d560986aa7fb360d353
Fabiha Hashmat <fhashmat@purdue.edu>
2025-06-19 00:00:30 -0400

    feat: added and tested extract_metadata() for CVE ID and published date

--- SecElf_StageC_PackageToCVE.py
+++ SecElf_StageC_PackageToCVE.py
@@ -2,6 +2,8 @@ import csv              # For reading and writing CSV files
 import requests         # For making HTTP requests to APIs/websites
 import time             # For sleep/delay to avoid rate limits
 import re               # For basic regex parsing
+import json             #read and parse the JSON files
+import os               #walking through directories with os.walk()
 
 # -----------------------------------------------------------------------------
 # Stage C: Package to CVE Mapping (SecElf_StageC_PackageToCVE.py)
@@ -13,13 +15,14 @@ import re               # For basic regex parsing
 #    - package_version
 #Accessing the resolved package column from the stage B
 #This code is also normalizing
-resolved_packages = [] # created an empty list to store all the resolved packages got from the stage B column resolved packages
-with open("library_packages.csv", "r") as f: # Open the Stage B CSV file 
-    reader = csv.DictReader(f) #Use DictReader to read the CSV specific column
-    for row in reader: # Loop through each row in the csv file
-        resolved = row.get("ResolvedPackage", "").strip() # remove any leading or trailing spaces
-        if resolved: #just storing the values of resolved packages if they are present
-            resolved_packages.append(resolved.lower())
+
+#resolved_packages = [] # created an empty list to store all the resolved packages got from the stage B column resolved packages
+#with open("library_packages.csv", "r") as f: # Open the Stage B CSV file 
+ #   reader = csv.DictReader(f) #Use DictReader to read the CSV specific column
+  #  for row in reader: # Loop through each row in the csv file
+   #     resolved = row.get("ResolvedPackage", "").strip() # remove any leading or trailing spaces
+    #    if resolved: #just storing the values of resolved packages if they are present
+     #       resolved_packages.append(resolved.lower())
 
 
 # FOR NOW THIS PART COULD BE SKIPPED. LATER IF WE NEED TO NORMALIZE MORE WE CAN DEFINE A FUNCTION HERE.
@@ -59,3 +62,26 @@ with open("library_packages.csv", "r") as f: # Open the Stage B CSV file
 #     - affected[].versions       -> List of version entries:
 #         - versions[].version    -> Specific version string (e.g., "1.1.1f")
 #         - versions[].status     -> Status (e.g., "affected", "unaffected", "under investigation")
+
+
+#Function 1 extract_metadata: extract cve_id and published date
+
+def extract_metadata(cve_json):
+    meta = cve_json.get("cveMetadata", {})
+    return {
+        "cve_id": meta.get("cveId", ""),
+        "published_date": meta.get("datePublished", "").split("T")[0]
+    }
+
+print(">>> About to read CVE file...")
+
+
+
+test_cve_file = "cvelistV5/cves/2024/0xxx/CVE-2024-0250.json"
+
+
+with open(test_cve_file, "r") as f:
+    data = json.load(f)
+    result = extract_metadata(data)
+    print(">>> Result:")
+    print(result)
